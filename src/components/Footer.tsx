d3436b7eba9e592b0ff739fc8ce0e9d5fe30d1b3
cuencaleanneth <cuencaleanneth@gmail.com>
2025-11-14 23:12:57 +0000

    cambio

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import './Footer.css';
 import motorllantasLogo from '../assets/img/motorllantas.png';
-import { FaFacebook, FaInstagram, FaTiktok, FaWhatsapp } from 'react-icons/fa';
+import { FaFacebook, FaInstagram, FaTiktok, FaWhatsapp, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => {
   return (
@@ -20,6 +20,7 @@ const Footer = () => {
           <a href="https://www.facebook.com/share/1DjvbkBZzx/?mibextid=wwXIfr" target="_blank" rel="noopener noreferrer"><FaFacebook /></a> 
           <a href="https://www.instagram.com/motorllantas.com_?igsh=cnR6NHBvMTVhazE0" target="_blank" rel="noopener noreferrer"><FaInstagram /></a> 
           <a href="https://www.tiktok.com/@motorllantas.com_?_r=1&_t=ZS-91P86eG2DA2" target="_blank" rel="noopener noreferrer"><FaTiktok /></a> 
+          <a href="https://youtube.com/@motorllantas?si=edIXnMRim1QJJYq1" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
         </div>
       </div>
     </footer>
